5132bdfa136e286dfaa9dc95e83fc2bf8b7f34e5
Omar <Omar@DESKTOP-OUKQKJG>
2024-12-11 14:56:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region variables
+
+            //int Number;
+            //Number = 5;
+            //Console.WriteLine(Number);
+
+            //int Number = 5;
+            //Console.WriteLine(Number);
+
+            //int Number =1 , Number2 = 2 , Number3 =3;
+
+            //Number = 7;
+
+            //Console.WriteLine(Number);
+            //int Number;
+
+            //Number = 12;
+
+            //Int32 Number02;
+
+            //Point P01;
+
+
+            //P01 = new Point();
+
+            //Point P02=new Point();
+            //P02.X = 12;
+
+            //Console.WriteLine(P01.X);
+            //Console.WriteLine(P01.Y);
+            //Console.WriteLine(P02.X);
+
+            //P01 = P02;
+            #endregion
+
+
+            //object O01 = new object();
+            //O01 = 1;
+            //Console.WriteLine(O01);
+            //Point P01 = new Point();
+            //Console.WriteLine(P01.GetType());
+            //P01.GetHashCode();
+            //Console.WriteLine(P01.GetHashCode());
+
+            //double X = 2.5;
+            //int Y = (int) X;
+            //Console.WriteLine(Y);
+
+            //long X = 1111111111111111111;
+            //int Y = (int)X;
+            //Console.WriteLine(Y);
+
+            //Console.WriteLine("enter your age");
+            //int Age;
+            //bool flag= int.TryParse(Console.ReadLine(),out Age );
+            //Console.WriteLine (Age + "agee is ");
+            //Console.WriteLine(flag);
+
+            //double X = 1.5;
+            //float Y = 1.5f;
+            //int o = 1_000_000_000;
+        }
+    }
+} 
